9c56cd7b9f31611f9215240d3cedc3471cc3e7aa
Iñaki Silanes <isilanes@gaepc36>
2009-09-29 15:59:45 +0200

    dupliback: full backup 1M -> 50D

--- dupliback.sh
+++ dupliback.sh
@@ -4,7 +4,7 @@
 DUPLICITY=/usr/bin/duplicity
 SOURCE=$HOME/
 DEST=ssh://b395676@backup.dreamhost.com//home/b395676/duplicity/flanders.home/
-FULLAGE=1M
+FULLAGE=50D
 VOLSIZE=100
 VERBOSITY=4
 EXCLUDES=$HOME/.myback/dupliback.excludes
@@ -14,7 +14,6 @@ export TMPDIR=$HOME/.myback/tmp
 export PASSPHRASE='e/%$13oqr31i5r652adlhqgb'
 
 # PW for SSH:
-#export SSH_AUTH_SOCK=`find /tmp/ssh* -name 'agent.*' -user isilanes`
 export SSH_AUTH_SOCK=`find /tmp/keyring-* -name 'socket.ssh' -user isilanes`
 
 # Ask to not suspend:
