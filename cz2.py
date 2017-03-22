7c0437de32d09b092bee2362398902f1f3f283c5
isilanes <isilanes@gmail.com>
2017-03-22 21:15:43 +0100

    some cleanup in cz2.py

--- cz2.py
+++ cz2.py
@@ -1,13 +1,30 @@
+# Standard libs:
 import lzma
 import time
 import pickle
 import multiprocessing as mp
 
+# Functions:
+def main():
+    """Main function."""
+
+    fn_in = "somefile"
+    fn_out = fn_in + ".xz"
+
+    with open(fn_out, "w") as fout:
+        with open(fn_in, 'rb') as fin:
+            CZ = ChopZip(fin, fout)
+            CZ.run()
+
+
+# Classes:
 class ChopZip(object):
+    """Class with all methods."""
+
+    CHUNK_SIZE = 10*1024*1024
+    NPROCS = 4
 
     def __init__(self, fin, fout):
-        self.chunk_size = 10*1024*1024
-        self.nprocs = 4
         self.fin = fin
         self.fout = fout
         self.procs = []
@@ -16,13 +33,17 @@ class ChopZip(object):
         self.compressed_chunks = {}
 
     def chunk_read(self):
+        """Iterable over data chunks read from input."""
+
         while True:
-            data = self.fin.read(self.chunk_size)
+            data = self.fin.read(self.CHUNK_SIZE)
             if not data:
                 break
             yield data
 
     def compress_chunk(self, chunk, i, lock):
+        """Take a single data chunk and compress it."""
+
         compressed_chunk = lzma.compress(chunk)
         lock.acquire()
         try:
@@ -30,65 +51,74 @@ class ChopZip(object):
                 d = pickle.load(f)
         except:
             d = {}
+
         d[i] = compressed_chunk
         with open("tmp", "w") as f:
             pickle.dump(d, f)
+
         lock.release()
-        print "chunk compressed:", i, [ x for x  in d ]
+        print("chunk compressed:", i, [x for x  in d])
 
     def run(self):
+        """Run the whole thing."""
+
         self.lock = mp.Lock()
 
         for chunk in self.chunk_read():
             # Feed a new process if pool low:
-            if len(self.procs) < self.nprocs:
-                p = mp.Process(target=self.compress_chunk, args=(chunk, self.index_read, self.lock))
-                p.daemon = True
-                p.start()
-                self.procs.append(p)
-                print "chunk read, index:", self.index_read
-                self.index_read += 1
+            if len(self.procs) < self.NPROCS:
+                self.feed_chunk_to_compress_queue(chunk)
 
             self.lock.acquire()
-            try:
-                with open("tmp", "rb") as f:
-                    self.compressed_chunks = pickle.load(f)
-            except:
-                self.compressed_chunks = {}
-            print "cycle", [ i for i in self.compressed_chunks ]
+            print("cycle", [i for i in self.compressed_chunks])
 
-            # Remove completed processes:
-            alive = []
-            for p in self.procs:
-                if p.is_alive():
-                    alive.append(p)
-            self.procs = alive[:]
+            # Remove completed processes from process list:
+            self.clean_proc_list()
 
             # Try to save to disk:
-            alive = {}
-            for i,chunk in self.compressed_chunks.items():
-                print i
-                if i == self.index_write:
-                    self.fout.write(chunk)
-                    self.index_write += 1
-                    print self.index_write
-                else:
-                    alive[i] = chunk
-            self.compressed_chunks = alive.copy()
+            self.save_ordered_chunks_to_disk()
 
-            with open("tmp", "w") as f:
-                pickle.dump(self.compressed_chunks, f)
             self.lock.release()
 
             time.sleep(1.0)
             #if len(self.procs) == self.nprocs:
             #    time.sleep(10.0)
 
+    def feed_chunk_to_compress_queue(self, chunk):
+        """Read next data chunk and feed it to compression loop."""
 
-fn_in = "somefile"
-fn_out = fn_in + ".xz"
+        p = mp.Process(target=self.compress_chunk, args=(chunk, self.index_read, self.lock))
+        p.daemon = True
+        p.start()
+        self.procs.append(p)
+        print("chunk read, index:", self.index_read)
+        self.index_read += 1
 
-with open(fn_out, "w") as fout:
-    with open(fn_in, 'rb') as fin:
-        CZ = ChopZip(fin, fout)
-        CZ.run()
+    def clean_proc_list(self):
+        """Remove completed processes from process list."""
+
+        #alive = []
+        #for p in self.procs:
+        #    if p.is_alive():
+        #        alive.append(p)
+        #self.procs = alive[:]
+        self.procs = [p for p in self.procs if p.is_alive()]
+
+    def save_ordered_chunks_to_disk(self):
+        """Take all compressed chunks in memory and save all the consecutive ones at the beginning."""
+
+        i = self.index_write
+        while True:
+            if i in self.compressed_chunks:
+                self.fout.write(self.compressed_chunks[i])
+                del self.compressed_chunks[i]
+                i += 1
+            else:
+                break
+
+        self.index_write = 1
+
+
+# Code:
+if __name__ == "__main__":
+    main()
